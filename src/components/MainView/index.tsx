212439e387d8524999414730adb4a78c7bf4d762
daniyar-yes <yeskaliyevdan@hotmail.com>
2025-04-02 18:52:25 -0700

    update config

--- src/components/MainView/index.tsx
+++ src/components/MainView/index.tsx
@@ -1,25 +1,25 @@
 import { useEffect, useState } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../../firebase';
-import mockCategories from '../../mock/CategoriesMock.json';
+// import mockCategories from '../../mock/CategoriesMock.json';
 
 const MainView = () => {
     const [categories, setCategories] = useState([]);
 
-    const categoriesObj = Object.keys(mockCategories)
-    console.log(categoriesObj)
+    // const categoriesObj = Object.keys(mockCategories)
+    // console.log(categoriesObj)
 
-    useEffect(() => {
-        if (categories.length == 0 && mockCategories) {
-        //   mockCategories.map()
-        }
-    }, [mockCategories])
+    // useEffect(() => {
+    //     if (categories.length == 0 && mockCategories) {
+    //     //   mockCategories.map()
+    //     }
+    // }, [mockCategories])
 
 
 
     useEffect(() => {
         const fetchData = async () => {
-          const querySnapshot = await getDocs(collection(db, "productsss"));
+          const querySnapshot = await getDocs(collection(db, "products"));
           const items = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
           setCategories(items);
         };
---
+++ src/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { secrets } from "./secrets";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: secrets.apiKey,
+  authDomain: secrets.authDomain,
+  projectId: secrets.projectId,
+  storageBucket: secrets.storageBucket,
+  messagingSenderId: secrets.messagingSenderId,
+  appId: secrets.appId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db }
